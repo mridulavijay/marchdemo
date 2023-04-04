6a4a99a7fbbea111cf3b0e8854ee8c91ee85835a
mridular <mridula.r@ictkerala.org>
2023-04-04 20:31:08 +0530

    first commit

---
+++ basics1.js
@@ -0,0 +1,46 @@
+// var a=['meera','nithya',10];
+// console.log(a[2]);
+// a.push('orange')
+// console.log(a);
+// a.pop();
+// console.log(a);
+// let car={
+//     model:'i10',
+//     color:'blue',
+//     brand:'Hyundai'
+// }
+// console.log(car.color);
+// var person=new Object();
+// person.name="Maya";
+// person.age=20;
+// console.log(person);
+
+// //Operators
+// var a=2;
+// var b=10;
+
+// console.log(a);
+// //Comparison Operators
+// if (a==b) {
+//     console.log('yes');
+// } else if(a>b) {
+//     console.log('a is greater than b');
+// }
+// else{
+//     console.log('a is less than b');
+// }
+
+// if(a==b || a==10){
+// console.log('a has a value 10 and is equal to b')
+// }
+// else{
+//     console.log('no');
+// }
+
+//Functions
+function addt(a,b) {//Function definition
+    var sum=a+b;
+    return sum;
+}
+var result=addt(6,7);//function call
+console.log(result);
